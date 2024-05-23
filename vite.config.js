42602f9d8db46797055ccb85cf5992201a692112
Mohamed Magdy <midomohamedmagdy@hotmail.com>
2024-05-23 05:35:51 +0300

    minor fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Space-tourism",
   plugins: [react()],
-  base: "/Space-tourism/"
 })
