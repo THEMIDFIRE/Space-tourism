11b50c8a7577f64b44b3ae70b094e46882f28451
Mohamed Magdy <midomohamedmagdy@hotmail.com>
2024-05-25 04:37:15 +0300

    Fixed active state problems

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
                     <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
                 </div>
                 <div className="homeBtn">
-                    <NavLink to="/Space-tourism/destination" className="btn">explore</NavLink>
+                    <NavLink to="/Space-tourism/destination/" className="btn">explore</NavLink>
                 </div>
             </section>
         </>
